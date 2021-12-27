c99ee93ec8c0f6024d84294ae5ab2292e9be0534
Katarina088 <katarina088@gmail.com>
2021-12-27 21:49:23 +0200

    add code.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let x = 5;
-let userName = 'katya';
-let cat = {
-  name: 'Bars',
-  age: 2
-};
+//let x = 5;
+//let userName = 'katya';
+//let cat = {
+ // name: 'Bars',
+  //age: 2
+//};
 
-////alert(typeof cat);
-alert( typeof(cat) );
+//alert(typeof cat);
+//alert( typeof(cat) );
 
 //alert( cat );
-console.log( cat );
-document.write ( userName );
-document.write( '<h2> userName <h2>' );
-document.write( '<h2>' + userName + '</h2>');
-document.write( `<h2>${userName}</h2>`);
+//console.log( cat );
+//document.write ( userName );
+//document.write( '<h2> userName <h2>' );
+//document.write( '<h2>' + userName + '</h2>');
+//document.write( `<h2>${userName}</h2>`);
+
+//доступ к элементу страницы
+//let container = document.querySelector('.container');
+//console.log(container);
+//container.innerHTML = `<h2>${userName}</h2>`;
+
+//let x = +prompt( 'Input number',0 );
+//x = +x;
+
+//alert( confirm( 'Продолжить?' ) );
+
